54dcd970655deb0ca60daef127f71cc3bd073b29
StanislavaGeorgieva <stanislava.georgieva.a58@learn.telerikacademy.com>
2024-11-04 17:44:52 +0200

    Services

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Benefits from './components/Benefits';
 import Colaboration from './components/Colaboration';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Services from './components/Services';
 
 const App = () => {
 
@@ -16,6 +17,7 @@ const App = () => {
         <Hero />
         <Benefits />
         <Colaboration />
+        <Services />
       </div>
       <ButtonGradient />
     </>
