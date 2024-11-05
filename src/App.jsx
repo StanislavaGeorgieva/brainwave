a57889211fec11d214cbbcdb200d474bd243d1ee
StanislavaGeorgieva <stanislava.georgieva.a58@learn.telerikacademy.com>
2024-11-05 12:21:05 +0200

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-
 import ButtonGradient from './assets/svg/ButtonGradient'
 import Benefits from './components/Benefits';
 import Colaboration from './components/Colaboration';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Pricing from './components/Pricing';
@@ -22,6 +22,7 @@ const App = () => {
         <Services />
         <Pricing />
         <Roadmap />
+        <Footer />
       </div>
       <ButtonGradient />
     </>
